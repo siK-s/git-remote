e7d54e7e00d9c20e0a362dae563aef90f5cfa85f
Atayi <sika@MacBook-Pro-de-Atayi.local>
2019-09-15 16:40:45 +0200

    Revert "Update comment"
    This reverts commit 8f98ae4ba4173a4836e4d638400666e6b36319f3.

--- wild.php
+++ wild.php
@@ -8,7 +8,7 @@
 
 <body>
    	<h1>Cette page wild.php de la branch wcs</h1>
-	<!--Ci dessous le texte ecrit sur la branche wcs -->
+	<!--Ci dessous le texte ecrit sur la branche master -->
 	<p>A la wild, on code en chaussette</p>
 </body>
 
